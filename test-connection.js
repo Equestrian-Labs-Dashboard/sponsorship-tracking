317388047530837f5266b7cbe7d042beaae2f10d
Equestrian Labs <arojas@corroshop.com>
2026-08-25 01:51:24 -0400

    Delete test-connection.js

--- test-connection.js
+++
@@ -1,4 +0,0 @@
-import {getBills,getLedger,getVendorBalance} from './src/services/googleSheets.js';
-console.log(await getBills().then(x=>`Bills: ${x.length}`));
-console.log(await getVendorBalance().then(x=>`Vendor: ${x.length}`));
-console.log(await getLedger().then(x=>`Ledger: ${x.length}`));

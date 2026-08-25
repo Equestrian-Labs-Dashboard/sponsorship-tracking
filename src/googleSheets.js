739a74b5e81538a5a109d8e450bfe2157abd71ea
Equestrian Labs <arojas@corroshop.com>
2026-08-25 02:31:57 -0400

    Update googleSheets.js

--- src/googleSheets.js
+++ src/googleSheets.js
@@ -1,39 +1,120 @@
 import { google } from "googleapis";
 
-export async function readSheet(name){
 
- const credentials = JSON.parse(
-   process.env.GOOGLE_SERVICE_ACCOUNT_JSON
- );
+export async function getSheetsList(){
 
- const auth = new google.auth.GoogleAuth({
-   credentials,
-   scopes:[
-    "https://www.googleapis.com/auth/spreadsheets.readonly"
-   ]
- });
+    const credentials =
+    JSON.parse(
+        process.env.GOOGLE_SERVICE_ACCOUNT_JSON
+    );
 
- const sheets = google.sheets({
-   version:"v4",
-   auth
- });
 
- const result = await sheets.spreadsheets.values.get({
-   spreadsheetId: process.env.GOOGLE_SHEET_ID,
-   range:`'${name}'!A:Z`
- });
+    const auth =
+    new google.auth.GoogleAuth({
 
- const rows=result.data.values || [];
+        credentials,
 
- if(rows.length<2) return [];
+        scopes:[
+            "https://www.googleapis.com/auth/spreadsheets.readonly"
+        ]
 
- const headers=rows[0];
+    });
 
- return rows.slice(1).map(row=>{
-   const obj={};
-   headers.forEach((h,i)=>{
-     obj[h]=row[i] || "";
-   });
-   return obj;
- });
+
+    const sheets =
+    google.sheets({
+        version:"v4",
+        auth
+    });
+
+
+    const result =
+    await sheets.spreadsheets.get({
+
+        spreadsheetId:
+        process.env.GOOGLE_SHEET_ID
+
+    });
+
+
+    return result.data.sheets.map(
+        s=>s.properties.title
+    );
+
+}
+
+
+
+export async function readSheet(sheetName){
+
+
+    const credentials =
+    JSON.parse(
+        process.env.GOOGLE_SERVICE_ACCOUNT_JSON
+    );
+
+
+    const auth =
+    new google.auth.GoogleAuth({
+
+        credentials,
+
+        scopes:[
+            "https://www.googleapis.com/auth/spreadsheets.readonly"
+        ]
+
+    });
+
+
+    const sheets =
+    google.sheets({
+        version:"v4",
+        auth
+    });
+
+
+
+    const result =
+    await sheets.spreadsheets.values.get({
+
+        spreadsheetId:
+        process.env.GOOGLE_SHEET_ID,
+
+
+        range:
+        `${sheetName}!A:Z`
+
+    });
+
+
+
+    const rows =
+    result.data.values || [];
+
+
+    if(rows.length < 2)
+        return [];
+
+
+
+    const headers =
+    rows[0];
+
+
+    return rows.slice(1).map(row=>{
+
+        let obj={};
+
+
+        headers.forEach((h,i)=>{
+
+            obj[h]=row[i] || "";
+
+        });
+
+
+        return obj;
+
+    });
+
 }
